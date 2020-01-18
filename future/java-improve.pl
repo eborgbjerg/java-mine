573064be734f87215632463f28a293c670f7fbf1
Olga Batrakova <olyalo0201@gmail.com>
2020-01-18 20:54:35 +0100

    java-improve work

--- future/java-improve.pl
+++ future/java-improve.pl
@@ -1,43 +1,57 @@
-#!/usr/bin/perl
-# $ ./java-improve.pl /home/admin/git/chessshell-api-1/
-#use strict;
+#!/usr/bin/perl -s
+# Example:
+# To list potential changes:
+# $ ./java-improve.pl           /home/admin/git/chessshell-api-1/
+# To actually make changes:
+# $ ./java-improve.pl -commit=1 /home/admin/git/chessshell-api-1/
+use strict;
 use warnings;
 use v5.22;
 use autodie;
 
+our $commit;
 
-# todo
-# rule to replace TAB with 4 spaces
-
-
+my %files_to_edit;
 
 my @source_dirs = @ARGV;
-
-use File::Copy;
 
 use File::Find;
 sub finder {
     if (/\.java\Z/) {
-        # todo - bail out if .bak file already exists!
-
         open my $fh, '<', $_;
         my @lines = <$fh>;
         close $fh;
-        &do_generics($_, \@lines);
+        &work_on_lines($_, \@lines);
     }
 }
 
 find(\&finder, @source_dirs);
 
-sub do_generics {
+say '=' x 80;
+say for keys %files_to_edit;
+say scalar keys %files_to_edit, " files in all.";
+
+=sub
+Make substitutions in one file.
+=cut
+sub work_on_lines {
     my ($filename, $lines) = @_;
-    if (grep(/new ArrayList<\w+>/, @$lines)) {
-        move($filename, "$filename.bak");  # todo  unless -f $filename
-        open my $fh, '>', $filename;
-        foreach my $line (@$lines) { ##  todo  generalize classname
-            $line =~ s/new ArrayList<\w+>(\s*[,;])/new ArrayList<>$1/;
-            print $fh $line;
+    my $line_count = 0;
+    open my $fh, '>', $filename if defined $commit;
+    foreach my $line (@$lines) {
+
+        # use a list of functions here
+
+        if ($line =~ s/ new \s+ (\w+) <\w+> \( \) (\s*[,;]) /new $1<>()$2/x) {
+            print "$filename: Generics: $line";
+            $line_count++;
         }
-        close $fh;
+        if ($line =~ s/ \t /    /x) {
+            print "$filename: TAB: $line";
+            $line_count++;
+        }
+        print $fh $line if defined $commit;
     }
+    close $fh if defined $commit;
+    $files_to_edit{$filename} = 1 if $line_count > 0;
 }
